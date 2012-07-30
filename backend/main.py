979a5b7cedac37e30a12710401f68304cff1c580
Brett Slatkin <brett@haxor.com>
2012-07-30 01:20:08 -0700

    Fixing expiration

--- backend/main.py
+++ backend/main.py
@@ -1163,7 +1163,7 @@ ROUTES = webapp.WSGIApplication([
 SESSION_OPTS = {
   'session.type': 'cookie',
   'session.key': '8bits',
-  'session.expires': False,
+  'session.cookie_expires': False,
   'session.validate_key': config.session_validate_key,
   'session.encrypt_key': config.session_encrypt_key,
 }
