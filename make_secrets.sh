801a7ec95aef002edc74487d591a197d85ea11cd
Brett Slatkin <brett@haxor.com>
2013-04-07 18:03:44 -0700

    Updating readme again

---
+++ make_secrets.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Copyright 2013 Brett Slatkin
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+TARGET=backend/secrets2.py
+
+if [ -e $TARGET ]
+then
+    echo "backend/secrets.py already exists! Doing nothing"
+else
+    touch $TARGET
+    echo "#!/usr/bin/env python" >> $TARGET
+    echo "session_encrypt_key = '$(openssl rand -base64 32)'" >> $TARGET
+    echo "session_validate_key = '$(openssl rand -base64 32)'" >> $TARGET
+fi
